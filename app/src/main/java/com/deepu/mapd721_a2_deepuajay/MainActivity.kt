0eba81bb9dde0d09b8f0971ade1cf9456b3b64a6
Deepu Ajay <deepuajay383@gmail.com>
2025-02-15 11:54:49 -0500

    Initial commit

---
+++ app/src/main/java/com/deepu/mapd721_a2_deepuajay/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.deepu.mapd721_a2_deepuajay
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deepu.mapd721_a2_deepuajay.ui.theme.MAPD721_A2_DeepuAjayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MAPD721_A2_DeepuAjayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MAPD721_A2_DeepuAjayTheme {
+        Greeting("Android")
+    }
+}
